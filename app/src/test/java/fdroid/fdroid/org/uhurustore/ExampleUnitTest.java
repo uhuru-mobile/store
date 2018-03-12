acf4c39dcf08b087602ee8dff94ae6202d6f8a01
Rafa Hernandez <rhernandez@teclib.com>
2018-03-12 18:40:58 -0400

    test(appDetail): validate select apk

--- app/src/test/java/fdroid/fdroid/org/uhurustore/ExampleUnitTest.java
+++ app/src/test/java/fdroid/fdroid/org/uhurustore/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package fdroid.fdroid.org.uhurustore;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void validateSelectAPKTest() throws Exception {
+        AppDetailsCtrl appDetailsCtrl = Mockito.mock(AppDetailsCtrl.class);
+
+        Mockito.when(appDetailsCtrl.selectAPK("com.test")).thenReturn(true);
+
+        Assert.assertEquals(true, appDetailsCtrl.selectAPK("com.test"));
     }
 }
